c02f6bb29bb104ca828f4b5562104a7bb705be70
hmhuan <minhhuanhuynh289@gmail.com>
2022-03-27 13:44:00 +0900

    handle multiple client by tokio spawn api

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
 
+use tokio::io::AsyncBufReadExt;
+use tokio::io::BufReader;
 use tokio::{
     net::TcpListener, 
-    io::{AsyncReadExt, AsyncWriteExt}
+    io::{AsyncWriteExt}
 };
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    let (mut socket, _addr) = listener.accept().await.unwrap();
+    loop {
+        let (mut socket, _addr) = listener.accept().await.unwrap();
 
-    let mut buffer = [0u8; 1024];
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
 
-    let bytes_read = socket.read(&mut buffer).await.unwrap();
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+    
+            loop {
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                if bytes_read == 0 {
+                    break;
+                }
+                writer.write_all(line.as_bytes()).await.unwrap();     
+                line.clear();   
+            }
+        });
 
-    socket.write_all(&buffer[..bytes_read]).await.unwrap();
-
+    }
 }
