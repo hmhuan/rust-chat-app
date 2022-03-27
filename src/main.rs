6a5b798746f61617b6d7c32451641b3d181bb495
hmhuan <minhhuanhuynh289@gmail.com>
2022-03-27 14:02:56 +0900

    broadcast message

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,21 @@ use tokio::io::AsyncBufReadExt;
 use tokio::io::BufReader;
 use tokio::{
     net::TcpListener, 
-    io::{AsyncWriteExt}
+    io::{AsyncWriteExt},
+    sync::broadcast,
 };
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
+    let (tx, _rx) = broadcast::channel::<String>(10);
+
     loop {
         let (mut socket, _addr) = listener.accept().await.unwrap();
+
+        let tx = tx.clone(); 
+        let mut rx = tx.subscribe();
 
         tokio::spawn(async move {
             let (reader, mut writer) = socket.split();
@@ -20,12 +26,18 @@ async fn main() {
             let mut line = String::new();
     
             loop {
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
-                if bytes_read == 0 {
-                    break;
-                }
-                writer.write_all(line.as_bytes()).await.unwrap();     
-                line.clear();   
+                tokio::select! {
+                    bytes_read = reader.read_line(&mut line) => {
+                        if bytes_read.unwrap() == 0 {
+                            break;
+                        }
+                        tx.send(line.clone()).unwrap();
+                        line.clear();
+                    },
+                    msg = rx.recv() => {
+                        writer.write_all(msg.unwrap().as_bytes()).await.unwrap();     
+                    }
+                }                
             }
         });
 
